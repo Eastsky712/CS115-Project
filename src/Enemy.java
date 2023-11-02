eb3ff7b8e89463c9fbfebbc28f0fc4ff1efdf715
Dongmin Seo <eastsky712@gmail.com>
2023-11-02 16:38:55 -0500

    -added basic attributes for player and enemy -removed different bullet classes and instead fused them into one

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Enemy {
+public class Enemy {
+    private int enemyHealth;
+    private int enemySpeed;
+    private int enemyX;
+    private int enemyY;
+    private static final int DEFAULT_ENEMY_HEALTH = 1;
+    private static final int DEFAULT_ENEMY_SPEED = 2;
+    private static final int DEFAULT_ENEMY_X = 100;
+    private static final int DEFAULT_ENEMY_Y = 100;
+    public Enemy(){
+        enemyHealth = DEFAULT_ENEMY_HEALTH;
+        enemySpeed = DEFAULT_ENEMY_SPEED;
+        enemyX = DEFAULT_ENEMY_X;
+        enemyY = DEFAULT_ENEMY_Y;
+    }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Player {
+public class Player {
+    private int playerHealth;
+    private int playerSpeed;
+    private int playerX;
+    private int playerY;
+    private static final int DEFAULT_PLAYER_HEALTH = 3;
+    private static final int DEFAULT_PLAYER_SPEED = 5;
+    private static final int DEFAULT_PLAYER_X = 0;
+    private static final int DEFAULT_PLAYER_Y = 0;
+    public Player(){
+        playerHealth = DEFAULT_PLAYER_HEALTH;
+        playerSpeed = DEFAULT_PLAYER_SPEED;
+        playerX = DEFAULT_PLAYER_X;
+        playerY = DEFAULT_PLAYER_Y;
+    }
 }
